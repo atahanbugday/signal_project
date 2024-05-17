fd8f2977f9f20088d20aa7a0e2022f7b1e6eed31
atahanbugday <atahanbugday@hotmail.com>
2024-05-17 20:58:02 +0200

    Javadoc comments added

--- src/main/java/com/cardio_generator/outputs/OutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/OutputStrategy.java
@@ -1,5 +1,19 @@
 package com.cardio_generator.outputs;
 
+
+/**
+ * Interface to output data
+ * Provides to output data to console/file/tcp/websocket
+ */
+
 public interface OutputStrategy {
+
+/**
+ * @param patientID ID for an individual patient (IDs are unique)
+ * @param timestamp the time when the data is recorded
+ * @param label shows the type of data (HeartRate etc.)
+ * @param data the data to output
+ */
+
     void output(int patientId, long timestamp, String label, String data);
 }
