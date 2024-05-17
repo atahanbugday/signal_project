1b5890a87962b1c7433b69be93708c04e4061036
atahanbugday <atahanbugday@hotmail.com>
2024-05-17 21:01:55 +0200

    Javadoc comments added

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -2,6 +2,16 @@ package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
 
+/**
+ * Creates an interface for data generator
+ */
+
 public interface PatientDataGenerator {
+
+/**
+ * @param patientId the ID of an individual patient
+ * @param outputStrategy  the output strategy to configure output
+ */
+
     void generate(int patientId, OutputStrategy outputStrategy);
 }
