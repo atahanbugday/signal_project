051ced6f25c2fffea3091ddf856b40a5f700c24d
atahanbugday <atahanbugday@hotmail.com>
2024-05-17 20:58:55 +0200

    Javadoc comments added

--- src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
@@ -7,6 +7,10 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * This class implements OutputStrategy for file output
+ */
+
 public class fileOutputStrategy implements OutputStrategy {
 
     private String baseDirectory; // changed variable name to camelCase 
@@ -14,10 +18,26 @@ public class fileOutputStrategy implements OutputStrategy {
     // changed variable name to UPPER_SNAKE_CASE since it is a constant
     public final ConcurrentHashMap<String, String> FILE_MAP = new ConcurrentHashMap<>(); 
 
+
+/**
+ * Constructor method
+ * it sets base directory for file output
+ * @param baseDirectory the base directory path
+ */
     public fileOutputStrategy(String baseDirectory) {
 
         this.baseDirectory = baseDirectory; // changed name to camelCase
     }
+
+
+/**
+ * Organizes data by labels and outputs formatted data to files
+ * @param patientID the ID of the patient
+ * @param timestamp the time when the data recorded 
+ * @param label the data label
+ * @param data the data to output
+ */
+
 
     @Override
     public void output(int patientId, long timestamp, String label, String data) {
@@ -29,6 +49,7 @@ public class fileOutputStrategy implements OutputStrategy {
             return;
         }
         // changed variable name to UPPER_SNAKE_CASE since it is a constant
+        //if file path is absent compute it, otherwise retrieve it
         String FilePath = FILE_MAP.computeIfAbsent(label, k -> Paths.get(BaseDirectory, label + ".txt").toString());
 
         // Write the data to the file
@@ -36,6 +57,7 @@ public class fileOutputStrategy implements OutputStrategy {
                 Files.newBufferedWriter(Paths.get(FilePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) {
             out.printf("Patient ID: %d, Timestamp: %d, Label: %s, Data: %s%n", patientId, timestamp, label, data);
         } catch (Exception e) {
+            //Use System.err.println to log errors
             System.err.println("Error writing to file " + FilePath + ": " + e.getMessage());
         }
     }
